55f8ed8ea4fd281ebe43aa1bc7957c8a5b7ed72e
DeepakPahawa <deepak.pahawa1911@gmail.com>
2025-04-17 23:44:30 +0530

    Portfolio Completed

---
+++ src/components/portfolio/Projects.tsx
@@ -0,0 +1,25 @@
+import React from "react";
+import { usePortfolioStore } from "@/store/usePortfolioStore";
+import { useAppStore } from "@/store/appStore";
+import { ProjectsTimelineWithAuth } from "portfolioui/job-jackpot";
+
+export const Projects = () => {
+  const {
+    saveProjectAndQualificationInfo,
+    portfolio: { projectsInfo, qualificationInfo },
+    isLoading,
+  } = usePortfolioStore();
+  const { isEditing } = useAppStore();
+  return (
+    <ProjectsTimelineWithAuth
+      isEditing={isEditing}
+      saveProjectAndQualificationInfo={saveProjectAndQualificationInfo}
+      projectsInfo={projectsInfo}
+      qualificationInfo={qualificationInfo}
+      isLoading={isLoading}
+      className="bg-white dark:bg-black"
+      circleClassName="bg-sky-400 dark:bg-sky-400"
+      lineClassName="from-sky-500 via-sky-500"
+    />
+  );
+};
